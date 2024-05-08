cf2fad009c9cf65a61a220b618cf8b2133389182
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-05-08 21:10:20 +0530

    initial setup

---
+++ first-responsive-website/src/App.js
@@ -0,0 +1,14 @@
+// short cut for react functional components 
+// rafce
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>this is react</h1>
+    </div>
+  )
+}
+
+export default App
