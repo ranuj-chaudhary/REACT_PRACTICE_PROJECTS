a67f20761ad91ea75a8c090f2ecdf40019564914
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-21 17:55:51 +0530

    navbar starter code

--- first-responsive-website/src/App.js
+++ first-responsive-website/src/App.js
@@ -10,13 +10,13 @@ import {
   Possibility,
   WhatGPT3,
 } from "./containers";
-
+import './App.css'
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
-        <Navbar />
-        <Header />
+      <Navbar />
+      <Header />
       </div>
       <Brand />
       <WhatGPT3 />
--- first-responsive-website/src/components/navbar/navbar.jsx
+++ first-responsive-website/src/components/navbar/navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line,  RiCloseLine } from "react-icons/ri";
 const navbar = () => {
   return (
-    <div>
-      navbar
+    <div className='gpt__navbar'>
+      <div className='gpt__navbar-links'>
+        <div className="gpt__navbar-links_logo">
+          <img src="" alt="" />
+        </div>
+        <RiMenu3Line size={18} color="white" 
+            className="menu-icon" />
+        <RiCloseLine  size={18} color="white" 
+            className="menu-icon"  />
+      </div>
+    
     </div>
   )
 }
