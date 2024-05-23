e50ef77fc21a7bb9d318319a175c30f58b6dc595
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-23 17:32:53 +0530

    brands sectoin below header

--- first-responsive-website/src/components/brand/Brand.jsx
+++ first-responsive-website/src/components/brand/Brand.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import {
+  googleLogo,
+  shopifyLogo,
+  slackLogo,
+  dropBoxLogo,
+  atlassianLogo,
+} from "./imports";
+
 const Brand = () => {
   return (
-    <div>
-      Brand
-      
+    <div className="gpt3__brands section__padding">
+      <div>
+        <img src={googleLogo} alt="google logo" />
+      </div>
+      <div>
+        <img src={slackLogo} alt="slack logo" />
+      </div>
+      <div>
+        <img src={atlassianLogo} alt="atlassian logo" />
+      </div>
+      <div>
+        <img src={dropBoxLogo} alt="dropbox logo" />
+      </div>
+      <div>
+        <img src={shopifyLogo} alt="shopify logo" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
