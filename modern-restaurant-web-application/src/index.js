6cc6447b76efde54e5a50607c76d86af0e3eb2e2
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-27 20:34:17 +0530

    navbar desktop, tablet, mobile layout

--- modern-restaurant-web-application/src/index.js
+++ modern-restaurant-web-application/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />)
