ed6a93768391a1fdc0d1fd1a1a4c1337245035bc
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-30 20:15:57 +0530

    lareles mobile tablet and desktop layout

--- modern-restaurant-web-application/src/container/Laurels/Laurels.jsx
+++ modern-restaurant-web-application/src/container/Laurels/Laurels.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import "./Laurels.css";
+import { images } from "../../constants/index";
+import { data } from "../../constants/index";
+const Laurels = () => {
+  const { laurels, spoon } = images;
+  const { awards } = data;
 
-import './Laurels.css';
+  return (
+    <div className="gericht__laurels section__padding">
+      <div className="gericht__laurels__awards">
+        <div className="gericht__laurels__heading">
+          <p>Awards & recognition</p>
+          <img src={spoon} alt="" />
+          <h2>Our Laurels</h2>
+        </div>
+        <div className="gericht__laurels__awards__list">
+          {awards.map((award, idx) => {
+            return (
+              <AwardsItems
+                title={award.title}
+                subtitle={award.subtitle}
+                imgUrl={award.imgUrl}
+                key={award.title + idx}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="gericht__laurels__image">
+        <img src={laurels} alt="" />
+      </div>
+    </div>
+  );
+};
 
-const Laurels = () => (
-  <div>
-    Laurels
-  </div>
-);
-
+function AwardsItems({ title, subtitle, imgUrl }) {
+  return (
+    <div className="gericht__laurels__listitem">
+      <img src={imgUrl} alt="" />
+      <div className="gericht__laurels__listitem__subheading">
+        <h5>{title}</h5>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+}
 export default Laurels;
