0b658c6e52f42958cf3a71f30c9e27d0b1d3d7ff
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-30 18:41:41 +0530

    video intro section with play pause

--- modern-restaurant-web-application/src/container/Intro/Intro.jsx
+++ modern-restaurant-web-application/src/container/Intro/Intro.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import { RiPauseLine, RiPlayFill } from "react-icons/ri";
 
-import './Intro.css';
+import { meal } from "../../constants/index";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [isVidoePlaying, setIsVideoPlaying] = useState(false);
+  const videoPlayer = useRef();
+  useEffect(
+    function () {
+      if (isVidoePlaying === true) {
+        videoPlayer.current.play();
+      } else {
+        videoPlayer.current.pause();
+      }
+    },
+    [isVidoePlaying]
+  );
+
+  return (
+    <div className="geritch__intro">
+      <video muted src={meal} ref={videoPlayer}></video>
+      <button type="button">
+        {isVidoePlaying ? (
+          <RiPauseLine
+            color="#ffffff"
+            size={24}
+            onClick={() => setIsVideoPlaying(false)}
+          />
+        ) : (
+          <RiPlayFill
+            color="#ffffff"
+            size={24}
+            onClick={() => setIsVideoPlaying(true)}
+          />
+        )}
+      </button>
+    </div>
+  );
+};
 
 export default Intro;
