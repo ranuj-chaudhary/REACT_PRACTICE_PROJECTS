b681943b3002b723de94552929b93572b8781de3
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-06-03 13:04:13 +0530

    replaced big size images with compressed images

--- first-responsive-website/src/containers/blog/imports.js
+++ first-responsive-website/src/containers/blog/imports.js
@@ -1,7 +1,7 @@
-import blog1 from  '../../assets/blog01.png'
-import blog2 from  '../../assets/blog02.png'
-import blog3 from  '../../assets/blog03.png'
-import blog4 from  '../../assets/blog04.png'
-import blog5 from  '../../assets/blog05.png'
+import blog1 from  '../../assets/blog01.jpg'
+import blog2 from  '../../assets/blog02.jpg'
+import blog3 from  '../../assets/blog03.jpg'
+import blog4 from  '../../assets/blog04.jpg'
+import blog5 from  '../../assets/blog05.jpg'
 
 export {blog1, blog2, blog3, blog4, blog5}
