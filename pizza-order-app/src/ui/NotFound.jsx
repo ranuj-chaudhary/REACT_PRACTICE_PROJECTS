74e3f0d049aae928ccb997063a70b8a45035875f
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-07-08 12:44:43 +0530

    git route error fix

--- pizza-order-app/src/ui/NotFound.jsx
+++ pizza-order-app/src/ui/NotFound.jsx
@@ -1,13 +1,16 @@
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, useRouteError } from "react-router-dom";
+import Button from "./Button";
 function NotFound() {
   const navigate = useNavigate();
+  let error = useRouteError();
 
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+    <div className="flex h-screen items-center justify-center bg-yellow-100">
+      <div className="container w-auto flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl">Something went wrong 😢. Go back enter order no again.</h1>
+        <p className="text-center" >(Order error status: {error.statusText}. Error Code: {error.status})</p>
+        <Button onClick={() => navigate(-1)}>&larr; Go back</Button>
+      </div>
     </div>
   );
 }
